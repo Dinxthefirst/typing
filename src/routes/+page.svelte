752a63dd77d8cd5378dbc131e2ab06ef933022fa
OliverToft <olivertoftk@live.dk>
2024-12-14 00:40:37 +0100

    Add setting for time between letters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
+  const emptyKey = "\u00A0";
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  const keyboardRows = ["qwertyuiop", "asdfghjkl", "zxcvbnm "];
-  let currentLetter = "";
-  let pressedKeys: string[] = [];
-  let score = 0;
-  let gameStarted = false;
-  const letterTimer = 5000;
+  const keyboardRows = ["qwertyuiop", "asdfghjkl", "zxcvbnm" + emptyKey];
+  let letterTimer = $state(1000);
+  let currentLetter = $state("");
+  let pressedKeys: string[] = $state([]);
+  let score = $state(0);
+  let gameStarted = $state(false);
 
   function handleKeydown(event: KeyboardEvent) {
     const key = event.key.toLowerCase();
@@ -15,10 +16,25 @@
       return;
     }
 
-    pressedKeys = [...pressedKeys, key];
+    pressedKeys.push(key);
 
-    key === currentLetter ? score++ : score--;
-    currentLetter = "";
+    if (currentLetter === emptyKey) {
+      return;
+    }
+
+    const isCorrectButton = key === currentLetter;
+
+    if (isCorrectButton) {
+      score++;
+    } else {
+      score--;
+    }
+
+    if (letterTimer != 0) {
+      setTimeout(nextLetter, letterTimer);
+    } else {
+      nextLetter();
+    }
   }
 
   function handleKeyup(event: KeyboardEvent) {
@@ -28,33 +44,88 @@
 
   function startGame() {
     gameStarted = true;
+    nextLetter();
+  }
+
+  function endGame() {
+    score = 0;
+    gameStarted = false;
+  }
+
+  function restartGame() {
+    endGame();
+    startGame();
+  }
+
+  function nextLetter() {
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
+    currentLetter = alphabet[randomIndex];
   }
 
   onMount(() => {
     window.addEventListener("keydown", handleKeydown);
     window.addEventListener("keyup", handleKeyup);
 
-    const interval = setInterval(() => {
-      if (!gameStarted) {
-        return;
-      }
-      const index = Math.floor(Math.random() * alphabet.length);
-      currentLetter = alphabet[index];
-    }, letterTimer);
-
     return () => {
       window.removeEventListener("keydown", handleKeydown);
       window.removeEventListener("keyup", handleKeyup);
-      clearInterval(interval);
     };
   });
+
+  function showLetterTimerTip(event: MouseEvent) {
+    console.log("showLetterTimerTip");
+    const tooltip = document.querySelector(".tooltip") as HTMLElement;
+    tooltip.style.display = "block";
+    tooltip.style.left = `${event.pageX + 10}px`;
+    tooltip.style.top = `${event.pageY + 10}px`;
+  }
+
+  function hideLetterTimerTip() {
+    const tooltip = document.querySelector(".tooltip") as HTMLElement;
+    tooltip.style.display = "none";
+  }
 </script>
 
 {#if !gameStarted}
-  <button on:click={startGame}>Start Game</button>
+  <div id="menu-container">
+    <button id="start-button" onclick={startGame}>Start Game</button>
+    <div id="slider-container">
+      <div
+        class="info"
+        onmouseout={hideLetterTimerTip}
+        onmousemove={showLetterTimerTip}
+      >
+        i
+      </div>
+      <div class="tooltip" style="display: none;">
+        Change how often the letters will appear in milliseconds
+      </div>
+      <input
+        type="number"
+        id="letter-timer-input"
+        min="0"
+        max="10000"
+        step="25"
+        bind:value={letterTimer}
+      />
+      <input
+        type="range"
+        id="letter-timer"
+        min="0"
+        max="10000"
+        step="25"
+        bind:value={letterTimer}
+      />
+    </div>
+  </div>
 {:else}
+  <div id="button-container">
+    <button id="restart-button" onclick={restartGame}>Restart Game</button>
+    <div>{letterTimer} ms</div>
+    <button id="main-menu-button" onclick={endGame}>Main Menu</button>
+  </div>
   <div id="score">Score: {score}</div>
-  <div id="current-letter">{currentLetter.toUpperCase() || "\u00A0"}</div>
+  <div id="current-letter">{currentLetter.toUpperCase() || emptyKey}</div>
   <div id="keyboard">
     {#each keyboardRows as row}
       <div class="keyboard-row">
@@ -62,7 +133,7 @@
           <div
             class="keyboard-key"
             class:pressed={pressedKeys.includes(key)}
-            class:empty-key={key === " "}
+            class:empty-key={key === emptyKey}
           >
             {key.toUpperCase()}
           </div>
@@ -79,6 +150,51 @@
     margin: 0;
     font-family: "Roboto", sans-serif;
     font-size: 1.5rem;
+  }
+
+  button {
+    padding: 1rem 2rem;
+    font-size: 1.5rem;
+    background-color: #f0f0f0;
+    border: none;
+    cursor: pointer;
+  }
+
+  #menu-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+    gap: 3rem;
+  }
+
+  #start-button:hover {
+    background-color: #e0e0e0;
+  }
+
+  #slider-container {
+    display: flex;
+    gap: 1rem;
+  }
+
+  #letter-timer-input {
+    width: 5rem;
+    text-align: center;
+    appearance: textfield;
+    border: 2px solid #f0f0f0;
+  }
+
+  #letter-timer {
+    width: 10rem;
+  }
+
+  #button-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    gap: 2rem;
   }
 
   #score {
@@ -100,6 +216,27 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  .info {
+    background-color: #f0f0f0;
+    border-radius: 50%;
+    width: 2rem;
+    height: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: default;
+  }
+
+  .tooltip {
+    position: absolute;
+    background-color: #f0f0f0;
+    width: 20rem;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    pointer-events: none;
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.25);
   }
 
   .keyboard-row {
